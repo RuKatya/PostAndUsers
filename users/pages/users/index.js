bc36465b758841fff555f5bae9c3cf98f1ef2782
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-07-12 18:25:14 +0300

    key

--- users/pages/users/index.js
+++ users/pages/users/index.js
@@ -9,27 +9,26 @@ export default function Post({ users }) {
         <MainLayout title={`Users`} >
             <div className="allUsers">
                 {users.map((user) => (
-                    <div key={user.id}>
-                        <Link href={{
-                            pathname: '/users/[user]',
-                            query: { user: user.id }
-                        }}>
-                            <a className="allUsers__links" >
-                                <Card
-                                    image='/imgs/tv.jpg'
-                                    header={user.name}
-                                    // meta='Friend'
-                                    description={user.username}
-                                    extra={<List>
-                                        <List.Item
-                                            icon='mail'
-                                            content={<a href={`mailto:${user.email}`}>{user.email}</a>}
-                                        />
-                                    </List>}
-                                />
-                            </a>
-                        </Link>
-                    </div>
+                    <Link href={{
+                        pathname: '/users/[user]',
+                        query: { user: user.id }
+                    }}
+                    key={user.id}>
+                        <a className="allUsers__list--links" >
+                            <Card
+                                image='/imgs/tv.jpg'
+                                header={user.name}
+                                // meta='Friend'
+                                description={user.username}
+                                extra={<List>
+                                    <List.Item
+                                        icon='mail'
+                                        content={<a href={`mailto:${user.email}`}>{user.email}</a>}
+                                    />
+                                </List>}
+                            />
+                        </a>
+                    </Link>
                 ))}
             </div>
 
